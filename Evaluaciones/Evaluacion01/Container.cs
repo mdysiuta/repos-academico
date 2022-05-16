fd1116f80abdf028d2371dc73fd01f0b3ac52ecc
mdysiuta <mote.dysiuta01@gmail.com>
2022-05-16 11:30:20 -0400

    Arreglada clase constructor.

--- Evaluaciones/Evaluacion01/Container.cs
+++ Evaluaciones/Evaluacion01/Container.cs
@@ -25,7 +25,7 @@ namespace Evaluacion01
 
         public Container() { }
 
-        public Container(string codigo, int capacidadMaxima = 0, int pesoActual = 0, byte tamaño = 20, string marca = "Sin especificar", bool esRefrigerado = false)
+        public Container(string codigo, int capacidadMaxima = 0, int pesoActual = 0, byte tamaño = 20, Buque buque = null, string marca = "Sin especificar", bool esRefrigerado = false)
         {
             if (codigo.Length < 5)
             {
@@ -42,6 +42,7 @@ namespace Evaluacion01
             // Si la condición no se cumple, el container será creado con el valor 20 por defecto
             if (tamaño >= 30) tamaño = 40;
 
+            this.buque = buque;
             this.marca = marca;
             this.esRefrigerado = esRefrigerado;
         }
