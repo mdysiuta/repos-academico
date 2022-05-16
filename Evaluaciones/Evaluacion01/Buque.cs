5761eb5115cb9d7d6463f5e225756f48732af0e3
mdysiuta <mote.dysiuta01@gmail.com>
2022-05-16 10:16:42 -0400

    Añadidas clases.

---
+++ Evaluaciones/Evaluacion01/Buque.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion01
+{
+    internal class Buque
+    {
+        private string codigo;
+        private string nombre;
+        private string pais;
+        private int cantidadContainers;
+        private int cantidadContainersCargados;
+        private int gastoTransporte;
+        private List<Container> listaContainers;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Pais { get => pais; set => pais = value; }
+        public int CantidadContainers { get => cantidadContainers; set => cantidadContainers = value; }
+        public int CantidadContainersCargados { get => cantidadContainersCargados; set => cantidadContainersCargados = value; }
+        public int GastoTransporte { get => gastoTransporte; set => gastoTransporte = value; }
+
+        public Buque() { }
+
+        public bool SubirContainer(Container container)
+        {
+            return false;
+        }
+    }
+}
---
+++ Evaluaciones/Evaluacion01/Container.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion01
+{
+    internal class Container
+    {
+        private string codigo;
+        private string marca;
+        private int capacidadMaxima;
+        private byte tamaño;
+        private bool esRefrigerado;
+        private int pesoActual;
+        public Buque buque;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public int CapacidadMaxima { get => capacidadMaxima; set => capacidadMaxima = value; }
+        public byte Tamaño { get => tamaño; set => tamaño = value; }
+        public bool EsRefrigerado { get => esRefrigerado; set => esRefrigerado = value; }
+        public int PesoActual { get => pesoActual; set => pesoActual = value; }
+
+        public Container() { }
+
+        public void SacarPeso() { }
+
+        public int ValorPagoInspeccion()
+        { 
+            return 0;
+        }
+
+        public int CalcularGastosEnvio()
+        {
+            return 0;
+        }
+    }
+}
